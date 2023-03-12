25831bca866ac9d8f06c065f086356f346959339
HHC <hh1183@gmail.com>
2023-03-12 20:50:11 +0900

    lemonade completed.

---
+++ Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -0,0 +1,137 @@
+package com.example.lemonade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lemonade.ui.theme.LemonadeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LemonadeTheme {
+                LemonApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonApp() {
+    // Current step the app is displaying
+    // (remember allows the state to be retained across recompositions.)
+    var currentStep by remember { mutableStateOf(1) }
+    // Number of times the lemon needs to be squeezed to turn into a glass of lemonade
+    var squeezeCount by remember { mutableStateOf(0) }
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+        when (currentStep) {
+            1 -> {
+                //Display lemon tree image and ask user to pick a lemon from the tree
+                LemonTreeAndImage(
+                    textLabelResourceId = R.string.lemon_select,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                    onImageClick = {
+                        // Update to next step
+                        currentStep = 2
+                        squeezeCount = (2..4).random()
+                    })
+            }
+            2 -> {
+                LemonTreeAndImage(
+                    textLabelResourceId = R.string.lemon_squeeze,
+                    drawableResourceId = R.drawable.lemon_squeeze,
+                    contentDescriptionResourceId = R.string.lemon_content_description,
+                    onImageClick = {
+                        // Decrease the squeeze count by 1 for each click the user performs
+                        squeezeCount--
+                        // When we're done squeezing the lemon, move to the next step.
+                        if (squeezeCount == 0)
+                            currentStep = 3
+                    })
+            }
+            3 -> {
+                // Display glass of lemonade image and ask user to drink the lemonade
+                LemonTreeAndImage(
+                    textLabelResourceId = R.string.lemon_drink,
+                    drawableResourceId = R.drawable.lemon_drink,
+                    contentDescriptionResourceId = R.string.lemon_content_description,
+                    onImageClick = {
+                        // Update to next Step
+                        currentStep = 4
+                    })
+            }
+            4 -> {
+                // Display empty glass image and ask user to start again.
+                LemonTreeAndImage(
+                    textLabelResourceId = R.string.lemon_empty_glass,
+                    drawableResourceId = R.drawable.lemon_restart,
+                    contentDescriptionResourceId = R.string.lemon_content_description,
+                    onImageClick = {
+                        // Back to starting step
+                        currentStep = 1
+                    })
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonTreeAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(textLabelResourceId),
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(drawableResourceId),
+            contentDescription = stringResource(contentDescriptionResourceId),
+            modifier = Modifier
+                .wrapContentSize()
+                .clickable(onClick = onImageClick)
+                .border(
+                    BorderStroke(2.dp, Color(105, 205, 216)),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .padding(16.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LemonadeTheme {
+        LemonApp()
+    }
+}
