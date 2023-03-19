1975c13601dce2d85249ac8d6dd82c3be8a1f14b
HHC <hh1183@gmail.com>
2023-03-19 15:11:41 +0900

    unit test completed.

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameUiState.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameUiState.kt
@@ -2,8 +2,8 @@ package com.example.android.unscramble.ui
 
 data class GameUiState(
     val currentScrambledWord: String = "",
-    val currentWordCount: Int = 0,
+    val currentWordCount: Int = 1,
     val isGuessedWordWrong: Boolean = false,
-    val score: Int = 1,
+    val score: Int = 0,
     val isGameOver: Boolean = false
 )
