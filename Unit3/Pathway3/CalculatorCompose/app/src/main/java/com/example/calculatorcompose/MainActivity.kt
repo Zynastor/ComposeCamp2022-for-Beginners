83a1309f2ed7c512722086e01bfa0b6307371eca
HHC <hh1183@gmail.com>
2023-04-06 21:41:15 +0900

    First Calculator

---
+++ Unit3/Pathway3/CalculatorCompose/app/src/main/java/com/example/calculatorcompose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.calculatorcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculatorcompose.ui.theme.CalculatorComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
+@Composable
+fun CalculatorApp(){
+    CalculatorComposeTheme {
+        
+    }
+}
